0037f6c2cc5d2cfadce9b7485cada9b66a3443c6
Daniel <dlrm72@gmail.com>
2016-08-09 00:57:57 -0500

    final home slides plus about page

---
+++ kurthutchinson-dot-com/App/site/controllers/HeadController.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+      .module('app')
+      .controller('HeadController', HeadController);
+
+    HeadController.$inject = ['$scope'];
+
+    function HeadController($scope) {
+        angular.extend($scope, {
+            dynamicCss: "div { outline: 1px solid red; }"
+        });
+
+        activate();
+
+        function activate() { }
+    }
+})();
